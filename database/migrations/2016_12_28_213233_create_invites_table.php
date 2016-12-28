a247afc9b1b0d611e7906090c0bb34d51f7f184e
Joe Anzalone <joe@shmit.com>
2016-12-28 16:35:13 -0500

    Generate Invites table with laracasts/Laravel-5-Generators-Extended: php artisan make:migration:schema create_invites_table --schema="\ code:string(32):unique, user_id:integer:unsigned:foreign:nullable, claimed_by:integer:unsigned:nullable, claimed_by:foreign:references('id'):on('users')"

---
+++ database/migrations/2016_12_28_213233_create_invites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code', 32)->unique();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('claimed_by')->unsigned()->nullable();
+            $table->foreign('claimed_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invites');
+    }
+}
