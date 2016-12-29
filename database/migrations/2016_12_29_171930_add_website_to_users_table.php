48132219e12d2cb6b92ab6da5254e3d83b380cec
Joe Anzalone <joe@shmit.com>
2016-12-29 12:19:51 -0500

    Add website to Users table with Laravel-5-Generators-Extended: php artisan make:migration:schema add_website_to_users_table --schema="website:string(255):nullable"

---
+++ database/migrations/2016_12_29_171930_add_website_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWebsiteToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('website', 255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('website');
+        });
+    }
+}
