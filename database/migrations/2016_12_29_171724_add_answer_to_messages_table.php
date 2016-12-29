126bfef61ceff4e2e088d96c6939c4cf0051af26
Joe Anzalone <joe@shmit.com>
2016-12-29 12:17:53 -0500

    Add answer to Messages table with Laravel-5-Generators-Extended: php artisan make:migration:schema add_answer_to_messages_table --schema="answer:text:nullable"

---
+++ database/migrations/2016_12_29_171724_add_answer_to_messages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAnswerToMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->text('answer')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropColumn('answer');
+        });
+    }
+}
