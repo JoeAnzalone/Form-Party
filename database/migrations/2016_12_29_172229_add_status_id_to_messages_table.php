b29b95b26eaed9b7d701cd06ab044670fd8809a1
Joe Anzalone <joe@shmit.com>
2016-12-29 12:22:48 -0500

    Add status_id to Messages table with Laravel-5-Generators-Extended: php artisan make:migration:schema add_status_id_to_messages_table --schema="status_id:integer:unsigned:default(0)"

---
+++ database/migrations/2016_12_29_172229_add_status_id_to_messages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStatusIdToMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->integer('status_id')->unsigned()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropColumn('status_id');
+        });
+    }
+}
