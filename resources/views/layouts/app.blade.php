bf400f3004014e5b4e2ce7b9dfb866eec6e176e5
Joe Anzalone <joe@shmit.com>
2016-12-30 05:34:30 -0500

    Add 🎉 emoji to layout <title> and header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ !empty($title) ? $title . ' :: ' : '' }}{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ !empty($title) ? $title . ' :: ' : '' }}{{ config('app.name', 'Laravel') }} 🎉</title>
 
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,600" rel="stylesheet" type="text/css">
@@ -38,7 +38,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                         🎉 {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
 
