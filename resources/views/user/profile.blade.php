d9cc7b9a084235812f4617209c3ad24abd5eaeba
Joe Anzalone <joe@shmit.com>
2016-12-31 00:00:46 -0500

    Make the 'Edit profile' link look better on narrower screen sizes

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -6,15 +6,18 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-body">
-                    <img class="user-avatar" src="{{ $user->avatar(200) }}" width="100" title="{{ $user->username_possessive }} avatar" alt="{{ $user->username_possessive }} avatar">
-                    <div class="user-info">
+                <div class="panel-body media user-info">
+                    <div class="media-left">
+                        <img class="user-avatar" src="{{ $user->avatar(200) }}" width="100" title="{{ $user->username_possessive }} avatar" alt="{{ $user->username_possessive }} avatar">
+                    </div>
+                    <div class="media-body">
                         <h3 class="user-username">{{ $user->username }}</h3>
                         @if ($user->website) <span class="separator">❥</span> <a class="user-website" target="_blank" href="{{ $user->website }}">{{ $user->website_without_protocol }}</a> @endif
                         <div class="user-joined">Partying since {{ $user->created_at->format('F Y') }}</div>
                         <div class="user-bio">{!! nl2br(e($user->bio)) !!}</div>
-                        @if ($user->is(Auth::user())) <a class="edit-user" href="{{ route('settings') }}">Edit profile</a> @endif
                     </div>
+
+                    @if ($user->is(Auth::user())) <a class="edit-user text-center" href="{{ route('settings') }}">Edit profile</a> @endif
                 </div>
             </div>
         </div>
