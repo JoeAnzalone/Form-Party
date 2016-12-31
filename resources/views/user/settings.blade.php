fdfe911a9f851c68262ce4209fe0c4e3d2ee67ac
Joe Anzalone <joe@shmit.com>
2016-12-30 23:09:46 -0500

    Add settings form to settings page

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -1,34 +1,103 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Profile</div>
                 <div class="panel-body">
-                    <p>Profile settings go here</p>
-                </div>
-            </div>
-        </div>
+                    <form class="form-horizontal" method="POST" action="{{ route('settings') }}">
+                        {{ method_field('PUT') }}
+                        {{ csrf_field() }}
 
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Account</div>
 
-                <div class="panel-body">
-                    <p>Account settings go here</p>
-                </div>
-            </div>
-        </div>
+                        <div class="panel panel-default">
+                        <div class="panel-heading text-center">Profile</div>
+                            <div class="panel-body">
+                                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                    <label for="name" class="col-md-4 control-label">Name</label>
 
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Notifications</div>
+                                    <div class="col-md-6">
+                                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name', $user->name) }}">
 
-                <div class="panel-body">
-                    <p>Notification settings go here</p>
+                                        @if ($errors->has('name'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('name') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('website') ? ' has-error' : '' }}">
+                                    <label for="website" class="col-md-4 control-label">Website</label>
+
+                                    <div class="col-md-6">
+                                        <input id="website" type="text" class="form-control" name="website" value="{{ old('website', $user->website) }}">
+
+                                        @if ($errors->has('website'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('website') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('bio') ? ' has-error' : '' }}">
+                                    <label for="bio" class="col-md-4 control-label">Bio</label>
+
+                                    <div class="col-md-6">
+                                        <textarea id="bio" class="form-control" name="bio">{{ old('bio', $user->bio) }}</textarea>
+
+                                        @if ($errors->has('bio'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('bio') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="panel panel-default">
+                            <div class="panel-heading text-center">Account</div>
+                            <div class="panel-body">
+
+                                <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                                    <label for="username" class="col-md-4 control-label">Username</label>
+
+                                    <div class="col-md-6">
+                                        <input id="username" type="text" class="form-control" name="username" value="{{ old('username', $user->username) }}" required>
+
+                                        @if ($errors->has('username'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('username') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                    <label for="email" class="col-md-4 control-label">E-Mail</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email', $user->email) }}" required>
+
+                                        @if ($errors->has('email'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
