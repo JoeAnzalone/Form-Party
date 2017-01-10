2d694048859a81a84060a52b4c61065d7a676328
Joe Anzalone <joe@shmit.com>
2017-01-10 02:13:47 -0500

    Add unread messages badge to hamburger menu icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,9 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
+                        @if (Auth::user()->unanswered_message_count)
+                            <span class="badge hamburger-menu-badge">{{ Auth::user()->unanswered_message_count }}</span>
+                        @endif
                     </button>
 
                     <!-- Branding Image -->
