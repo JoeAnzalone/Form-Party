325f5058594c101dc11671604d297c86778f5da7
Joe Anzalone <joe@shmit.com>
2017-01-09 23:51:37 -0500

    Add answered_at column to Messages table using Laravel-5-Generators-Extended: php artisan make:migration:schema add_answered_at_to_messages_table --schema="answered_at:dateTime:nullable"

---
+++ database/migrations/2017_01_10_045040_add_answered_at_to_messages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAnsweredAtToMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dateTime('answered_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropColumn('answered_at');
+        });
+    }
+}
