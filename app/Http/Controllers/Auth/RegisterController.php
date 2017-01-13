416b1dc6065beaaa783fa86f22ad486e4025b6fc
Joe Anzalone <joe@shmit.com>
2017-01-12 23:34:34 -0500

    Keep users logged in for five years after registering 1. Copy vendor/laravel/framework/src/Illuminate/Foundation/Auth/RegistersUsers.php@register to app/Http/Controllers/Auth/RegisterController.php 2. Add the `true` parameter to guard()->login() to keep the user logged in

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -100,4 +101,22 @@ class RegisterController extends Controller
 
         return view('auth.register', ['title' => 'Register', 'invite' => $invite, 'info' => $alert]);
      }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        $this->guard()->login($user, true);
+
+        return $this->registered($request, $user)
+            ?: redirect($this->redirectPath());
+    }
 }
