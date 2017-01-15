a2727f8d9b5315349313b866a936a87589f22fa0
Joe Anzalone <joe@shmit.com>
2017-01-15 15:38:38 -0500

    Generate InvitePolicy: php artisan make:policy InvitePolicy --model=Invite

---
+++ app/Policies/InvitePolicy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Invite;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class InvitePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the invite.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Invite  $invite
+     * @return mixed
+     */
+    public function view(User $user, Invite $invite)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create invites.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the invite.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Invite  $invite
+     * @return mixed
+     */
+    public function update(User $user, Invite $invite)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the invite.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Invite  $invite
+     * @return mixed
+     */
+    public function delete(User $user, Invite $invite)
+    {
+        //
+    }
+}
